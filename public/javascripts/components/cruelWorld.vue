b78b6ad58eb2c17a9b9afa362e48a4c26aafd03d
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-05-07 18:48:56 +0100

    Set up vue server reload-on-change

---
+++ public/javascripts/components/cruelWorld.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="cruel">
+        Cruel
+        <span>{{ who }}</span>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+        data () {
+            return {
+                who: 'world'
+            }
+        },
+        mounted () {
+            setTimeout(() => {
+                console.log('Use ES6 too');
+            }, 1000);
+        }
+    }
+</script>
+
+<style scoped>
+    .cruel {
+        color: green;
+    }
+</style>
