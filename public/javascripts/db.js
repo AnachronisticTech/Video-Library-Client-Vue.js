49db0990ffebfcd8b8dbad6f4d31094e0a62e29a
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-05-07 20:26:00 +0100

    Cleaned branch

--- public/javascripts/db.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: 'hhtp://localhost:3000'
-});
-
-exports.video_detail = function(id) {
-    return instance.get('/video/'+id);
-}
--- public/javascripts/main.js
+++
@@ -1,8 +0,0 @@
-var Vue = require('vue/dist/vue.js');
-
-Vue.component('cruelWorld', require('./components/cruelWorld.vue'));
-Vue.component('videoElement', require('./components/videoElement.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
