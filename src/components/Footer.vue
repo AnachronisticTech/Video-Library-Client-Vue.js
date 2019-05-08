eb1cb2b5c084d9ac2901d406412b0b7173fc724f
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-05-08 18:44:50 +0100

    Updated download bar visuals

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,19 +1,24 @@
 <template>
     <div class="footer">
-        <div class="right">
-            <input type="text" placeholder="https://youtube.com/watch" v-bind:style="{width: width}">
+        <div class="left">
             <button class="add" v-on:click="toggleWidth">
                 <img src="@/assets/add_icon.png" />
             </button>
+            <div class="hex-box">
+                <input type="text" placeholder="https://youtube.com/watch" v-bind:style="{width: width}"  >
+                <div class="hexagon"></div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import db from '@/db.js'
     export default {
         data() {
             return {
-                width:'0px'
+                width:'0px',
+                link:''
             }
         },
         methods: {
@@ -23,44 +28,29 @@
                 } else {
                     this.width = '0px';
                 }
-            }
+            },
+            // getVideo: function(event) {
+            //     if (this.link != '') {
+            //         db.post('/library/video/create', this.link)
+            //     }
+            // }
         }
     }
 </script>
 
 <style scoped>
     .footer {
-        /* background:yellow; */
         width:100%;
         height:120px;
         position:absolute;
         bottom:0;
-        overflow-y:hidden;
-    }
-    .right {
-        position:absolute;
-        right:20px;
-        height:100%;
-    }
-    .right input {
-        height:60px;
-        margin-top:30px;
-        margin-bottom:30px;
-        display:block;
-        position:absolute;
-        right:60px;
-        border:0;
-        border-radius:5px;
-        filter: drop-shadow(0px 0px 10px gray);
-        font-size:20px;
-        padding-left:20px;
-        transition:width 0.5s ease;
+        text-align:left;
     }
     .add {
         height:120px;
         width:120px;
         padding:0;
-        margin:0;
+        margin-left:10px;
         border:0;
         background:rgba(0,0,0,0);
         outline:none;
@@ -69,5 +59,44 @@
         width:100%;
         height:100%;
         filter: drop-shadow(0px 0px 10px gray);
+    }
+    .hex-box {
+        position:absolute;
+        left:100px;
+        bottom:00px;
+        filter: drop-shadow(0px 0px 10px gray);
+        z-index:-1;
+    }
+    .hex-box input {
+        height:60px;
+        margin-top:30px;
+        margin-bottom:30px;
+        display:block;
+        border:0;
+        font-size:20px;
+        padding-left:40px;
+        padding-right:20px;
+        transition:width 0.5s ease;
+        float:left;
+    }
+    .hexagon {
+        position: relative;
+        width: 62px; 
+        height: 35.80px;
+        background-color: transparent;
+        margin: 17.90px 0;
+        transform:rotate(90deg);
+        float:left;
+        z-index:-1;
+    }
+    .hexagon:before {
+        content: "";
+        position: absolute;
+        width: 0;
+        left:25px;
+        top:31px;
+        border-bottom: 17.90px solid white;
+        border-left: 31px solid transparent;
+        border-right: 31px solid transparent;
     }
 </style>
