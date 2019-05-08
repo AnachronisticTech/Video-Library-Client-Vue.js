9531ef889f13ad3c3ca290048a322c103d14c166
Daniel <danielmarriner@anachronistic-tech.co.uk>
2019-05-08 21:44:05 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,40 +1,40 @@
 <template>
-	<div class="about">
-		<h1>Video Library</h1>
-			<div class="body">
-				<p>The <i>Video Library</i> project is a web based interface to manage videos on a local device.</p>
-				<p>I created this project in order to accomplish two main goals: </p>
-				<ul>
-					<li>An easy way to add new videos to a single location</li>
-					<li>A way to sort videos by tags which easily identify them by their characteristics</li>
-					<li>A way to view the new videos within the same program</li>
-				</ul>
-				<!--	<img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/itunes/macos-mojave-ios12-macbook-iphone-x-itunes-store-hero.jpg">-->
-			</div>
+    <div class="about">
+        <h1>Video Library</h1>
+            <div class="body">
+                <p>The <i>Video Library</i> project is a web based interface to manage videos on a local device.</p>
+                <p>I created this project in order to accomplish two main goals: </p>
+                <ul>
+                    <li>An easy way to add new videos to a single location</li>
+                    <li>A way to sort videos by tags which easily identify them by their characteristics</li>
+                    <li>A way to view the new videos within the same program</li>
+                </ul>
+                <!--    <img src="https://support.apple.com/library/content/dam/edam/applecare/images/en_US/itunes/macos-mojave-ios12-macbook-iphone-x-itunes-store-hero.jpg">-->
+            </div>
 
-		<h2>Technologies Used</h2>
-			<div class="body">
-			<p>For this project, I chose to use some of the more modern technologies of today, such as NodeJS and Vue.JS in order to create a web-based application. I chose to create a web-based application as desktop applications tend to be unreliable and very platform specific. In addition, since I am already confident with web-based programming languages, such as JavaScript, HTML and CSS, it would be much quicker for me to create a web-based application as opposed to a desktop-based one.</p>
-			<p>I also chose a web-based application due to the fact that modern solutions, such as VSCode, Atom and even GitHub Desktop are powered by electron. This usage of server-based web technologies which can be used in desktop applications makes it easier to <b>TODO: fill this section out</b></p>
+        <h2>Technologies Used</h2>
+            <div class="body">
+            <p>For this project, I chose to use some of the more modern technologies of today, such as NodeJS and Vue.JS in order to create a web-based application. I chose to create a web-based application as desktop applications tend to be unreliable and very platform specific. In addition, since I am already confident with web-based programming languages, such as JavaScript, HTML and CSS, it would be much quicker for me to create a web-based application as opposed to a desktop-based one.</p>
+            <p>I also chose a web-based application due to the fact that modern solutions, such as VSCode, Atom and even GitHub Desktop are powered by electron. This usage of server-based web technologies which can be used in desktop applications makes it easier to <b>TODO: fill this section out</b></p>
 
 
-			</div>
-		<hr>
-		<footer>
-			Created by Daniel Marriner &copy; All Rights Reserved
-		</footer>
-	</div>
+            </div>
+        <hr>
+        <footer>
+            Created by Daniel Marriner &copy; All Rights Reserved
+        </footer>
+    </div>
 </template>
 
 <style>
-	.body {
-		text-align: left;
-		/*column-count: 2;*/
-		margin-left: 10%;
-		margin-right: 10%;
-	}
+    .body {
+        text-align: left;
+        /*column-count: 2;*/
+        margin-left: 10%;
+        margin-right: 10%;
+    }
 
-	img {
-		width: 100%;
-	}
+    img {
+        width: 100%;
+    }
 </style>
